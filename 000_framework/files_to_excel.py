2e3b5c631c1a53c4c92e59e650059700f56dbf77
Billour Ou <billourcode@gmail.com>
2024-04-15 07:36:58 +0800

    create a lot of new folder 001_linux

---
+++ 000_framework/files_to_excel.py
@@ -0,0 +1,48 @@
+'''
+Title: List All files of the folder, this program will get each files name in folder and exporting to the Excel.
+Author : Billour Ou(歐育溙)
+\\020_IEEEE論文20240319下載\\
+Date : 2024/4/14
+Verstion: 2024041401
+#pip3 install pandas
+#pip3 install openpyxl
+'''
+
+import os
+import sys
+import pandas as pd
+
+#sys.set_int_max_str_digits(0)
+excel_path = u"D:\\OneDrive\\000雲科大研究所相關\\0 李泰興教授 碩班 011_112-1高速數位電路訊號完整度\\研究生報告進度20240412\\論文列表1.xlsx"
+directory = u"D:\\OneDrive\\000雲科大研究所相關\\0 李泰興教授 碩班 011_112-1高速數位電路訊號完整度\\研究生報告進度20240412\\020_IEEEE論文20240319下載\\"
+
+files = []
+class ls_files:
+    def __init__(self) -> None:
+        pass
+    def e_list(file_name):
+        with open(file_name, "r", encoding='UTF-8') as f :
+            data = f.read()
+
+    def main():
+        try:
+            folder_path = directory
+            for file_name in os.listdir(folder_path):
+                files.append(file_name)
+                #print(  file_name+" \n")
+            data = {u'論文名稱': files }
+
+            df = pd.DataFrame(data)
+            #print(df)
+            #print("Length of all PDFs : " + str(len(file_name)))
+            df.to_excel(excel_path, index=False)
+        finally:
+            print("All Done. Listing all files into the Excel.")
+
+
+if __name__ == "__main__":
+    ls_files.main()
+
+
+
+        
